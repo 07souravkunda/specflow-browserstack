4b2e80050bd5254f185ca7dac26bca6a1f759d1a
Sourav Kunda <50537259+07souravkunda@users.noreply.github.com>
2024-08-23 11:26:51 +0530

    add: mstest

--- SpecFlow_BrowserStack/resources/features/SampleLocalTest.feature
+++
@@ -1,5 +0,0 @@
-@sample-local-test
-Feature: BStack Local
-	Scenario Outline: Open BrowserStack Local
-		Given I navigate to local website
-		Then title should contain BrowserStack Local
--- SpecFlow_BrowserStack/resources/src/SampleTest.cs
+++ SpecFlow_BrowserStack/resources/src/SampleTest.cs
@@ -1,63 +1,65 @@
-using TechTalk.SpecFlow;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using OpenQA.Selenium;
-using System;
+using OpenQA.Selenium.Remote;
 using OpenQA.Selenium.Support.UI;
-using NUnit.Framework;
 using SeleniumExtras.WaitHelpers;
-using System.Threading;
+using SpecFlowBrowserStack;
+using TechTalk.SpecFlow;
 
-namespace SpecFlowBrowserStack
+
+
+namespace SpecFlowBasics.StepDefinitions
 {
-	[Binding]
-	public class SampleTest
+    [Binding]
 
-	{
-		private readonly IWebDriver _driver;
+    public class SampleTest
+    {
+        private readonly IWebDriver _driver;
 		private string? productOnPageText;
 		private string? productOnCartText;
 		private bool? cartOpened;
 		readonly WebDriverWait wait;
+			public SampleTest(RemoteWebDriver driver)
+			{
+				_driver = BrowserStackSpecFlowTest.ThreadLocalDriver.Value;
+				wait = new WebDriverWait(_driver, TimeSpan.FromSeconds(30));
+			}
 
-		public SampleTest()
-		{
-			_driver = BrowserStackSpecFlowTest.ThreadLocalDriver.Value;
-			wait = new WebDriverWait(_driver, TimeSpan.FromSeconds(30));
-		}
+      [Given(@"I navigate to website")]
+			public void GivenINavigatedTowebsite()
+			{
+				_driver.Navigate().GoToUrl("https://bstackdemo.com");
+			}
 
-		[Given(@"I navigate to website")]
-		public void GivenINavigatedTowebsite()
-		{
-			_driver.Navigate().GoToUrl("https://bstackdemo.com");
-		}
+			[Then(@"I should see title (.*)")]
+			public void ThenIShouldSeeTitle(string title)
+			{
+				Thread.Sleep(5000);
+				Assert.IsTrue(_driver.Title.Equals(title));
+			}
+			
+			[Then(@"I add product to cart")]
+			public void ThenIAddProductToCart()
+			{
+				wait.Until(ExpectedConditions.ElementExists(By.XPath("//*[@id=\"1\"]/p")));
+				productOnPageText = _driver.FindElement(By.XPath("//*[@id=\"1\"]/p")).Text;
+				wait.Until(ExpectedConditions.ElementExists(By.XPath("//*[@id=\"1\"]/div[4]")));
+				_driver.FindElement(By.XPath("//*[@id=\"1\"]/div[4]")).Click();
+			}
 
-		[Then(@"I should see title (.*)")]
-		public void ThenIShouldSeeTitle(string title)
-		{
-			Thread.Sleep(5000);
-			Assert.That(_driver.Title, Is.EqualTo(title));
-		}
-		
-		[Then(@"I add product to cart")]
-		public void ThenIAddProductToCart()
-		{
-			wait.Until(ExpectedConditions.ElementExists(By.XPath("//*[@id=\"1\"]/p")));
-			productOnPageText = _driver.FindElement(By.XPath("//*[@id=\"1\"]/p")).Text;
-			wait.Until(ExpectedConditions.ElementExists(By.XPath("//*[@id=\"1\"]/div[4]")));
-			_driver.FindElement(By.XPath("//*[@id=\"1\"]/div[4]")).Click();
-		}
+			[When(@"I check if cart is opened")]
+			public void ThenICheckIfCartIsOpened()
+			{
+				cartOpened = _driver.FindElement(By.XPath("//*[@class=\"float-cart__content\"]")).Displayed;
+				Assert.IsTrue(cartOpened);
+			}
 
-		[When(@"I check if cart is opened")]
-		public void ThenICheckIfCartIsOpened()
-		{
-			cartOpened = _driver.FindElement(By.XPath("//*[@class=\"float-cart__content\"]")).Displayed;
-			Assert.That(cartOpened, Is.EqualTo(true));
-		}
+			[Then(@"I should see same product in cart")]
+			public void ThenIShouldSeeSameProductInCart()
+			{
+				productOnCartText = _driver.FindElement(By.XPath("//*[@id=\"__next\"]/div/div/div[2]/div[2]/div[2]/div/div[3]/p[1]")).Text;
+				Assert.IsTrue(productOnCartText.Equals(productOnPageText));
+			}
 
-		[Then(@"I should see same product in cart")]
-		public void ThenIShouldSeeSameProductInCart()
-		{
-			productOnCartText = _driver.FindElement(By.XPath("//*[@id=\"__next\"]/div/div/div[2]/div[2]/div[2]/div/div[3]/p[1]")).Text;
-			Assert.That(productOnCartText, Is.EqualTo(productOnPageText));
-		}
-	}
+    }
 }
